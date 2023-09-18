bed69c667a439b62e4b5f5d43ecd421eaea86f58
Peter Palanjian <ppalanji@usc.edu>
2023-09-17 19:16:48 -0700

    Initial commit

---
+++ app/src/main/java/com/usc/minesweeper/MainActivity.java
@@ -0,0 +1,81 @@
+package com.usc.minesweeper;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int COLUMN_COUNT = 2;
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cell_tvs = new ArrayList<TextView>();
+
+        // Method (2): add four dynamically created cells
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
+        for (int i = 0; i<12; i++) {
+            for (int j=0; j<10; j++) {
+                TextView tv = new TextView(this);
+                tv.setHeight( dpToPixel(32) );
+                tv.setWidth( dpToPixel(32) );
+                tv.setTextSize( 16 );//dpToPixel(32) );
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
+    }
+
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    public void onClickTV(View view){
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        int i = n/COLUMN_COUNT;
+        int j = n%COLUMN_COUNT;
+        tv.setText(String.valueOf(i)+String.valueOf(j));
+        if (tv.getCurrentTextColor() == Color.GREEN) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        }else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
+    }
+}
