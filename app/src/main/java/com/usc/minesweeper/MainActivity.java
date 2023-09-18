084029bc4eda08f76da56f9d48775d528dea558f
Peter Palanjian <ppalanji@usc.edu>
2023-09-17 20:30:33 -0700

    Application now randomly generates mine locations and places them throughout the grid.

--- app/src/main/java/com/usc/minesweeper/MainActivity.java
+++ app/src/main/java/com/usc/minesweeper/MainActivity.java
@@ -2,6 +2,7 @@ package com.usc.minesweeper;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -10,14 +11,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int COLUMN_COUNT = 10;
+    private static final int ROW_COUNT = 12;
 
+    private static final int NUMBER_OF_BOMBS = 4;
+    private static int BOMBS_LEFT = NUMBER_OF_BOMBS;
     // save the TextViews of all cells in an array, so later on,
     // when a TextView is clicked, we know which cell it is
     private ArrayList<TextView> cell_tvs;
+    private boolean[][] bombLocation;
+
+    //"State" variable, whether or not user is in flag mode
+    public static boolean flagMode = false;
+
 
     private int dpToPixel(int dp) {
         float density = Resources.getSystem().getDisplayMetrics().density;
@@ -30,11 +40,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cell_tvs = new ArrayList<TextView>();
-
+        initializeBombs();
         // Method (2): add four dynamically created cells
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
-        for (int i = 0; i<12; i++) {
-            for (int j=0; j<10; j++) {
+        for (int i = 0; i<ROW_COUNT; i++) {
+            for (int j=0; j<COLUMN_COUNT; j++) {
                 TextView tv = new TextView(this);
                 tv.setHeight( dpToPixel(32) );
                 tv.setWidth( dpToPixel(32) );
@@ -56,6 +66,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void initializeBombs() {
+        Random rand = new Random(System.currentTimeMillis());
+        bombLocation = new boolean[ROW_COUNT][COLUMN_COUNT];
+        for(int i=0; i<NUMBER_OF_BOMBS; ++i){
+            int bombIndex = rand.nextInt(ROW_COUNT * COLUMN_COUNT);
+            int bombRow = bombIndex/COLUMN_COUNT;
+            int bombColumn = bombIndex % COLUMN_COUNT;
+            Log.d("Debugger", "Bomb #" + i + " is placed at [" + bombRow + "][" + bombColumn + "]");
+            if(bombLocation[bombRow][bombColumn]) --i;
+            else bombLocation[bombRow][bombColumn] = true;
+        }
+    }
+
     private int findIndexOfCellTextView(TextView tv) {
         for (int n=0; n<cell_tvs.size(); n++) {
             if (cell_tvs.get(n) == tv)
@@ -69,12 +92,21 @@ public class MainActivity extends AppCompatActivity {
         int n = findIndexOfCellTextView(tv);
         int i = n/COLUMN_COUNT;
         int j = n%COLUMN_COUNT;
-        tv.setText(String.valueOf(i)+String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GREEN) {
-            tv.setTextColor(Color.GRAY);
-            tv.setBackgroundColor(Color.LTGRAY);
-        }else {
-            //do nothing
+        //tv.setText(String.valueOf(i)+String.valueOf(j));
+        //tv.setTextColor(Color.GRAY);
+
+        //redundant code
+        if (tv.getCurrentTextColor() == Color.GREEN && !flagMode) {
+            if(bombLocation[i][j]){
+                tv.setBackgroundColor(Color.RED);
+                BOMBS_LEFT--;
+            }
+            else{
+                tv.setBackgroundColor(Color.LTGRAY);
+            }
+        }
+        else if (tv.getCurrentTextColor() == Color.GRAY && flagMode){
+            //implement flag functionality
         }
     }
 }
